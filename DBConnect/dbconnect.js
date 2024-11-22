93386b4ad8865d82bda2524cffd58fbe7ee2e9e4
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-11-22 11:28:04 +0600

    DB connected

--- DBConnect/dbconnect.js
+++ DBConnect/dbconnect.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-function DBConnect() {
-  mongoose
-    .connect(
-      "mongodb+srv://alimscohfw:Tumi@Amar*84@cluster0.yfgu72u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    )
-    .then(() => {
-      console.log("Database is Connected");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+// function DBConnect() {
+//   mongoose
+//     .connect(
+//       "mongodb+srv://alimscohfw:Tumi@Amar*84@cluster0.yfgu72u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//     )
 
-module.exports = DBConnect;
+//     .then(() => {
+//       console.log("Database is Connected");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+// module.exports = DBConnect;
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const DBConnect = require("./DBConnect/dbconnect");
+const mongoose = require("mongoose");
 const crudModel = require("./models/crudModel");
 const app = express();
-DBConnect();
+
 app.use(express.json());
 const port = 4000;
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/crud");
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db is not connected");
+    console.log(error.massage);
+    process.exit(1);
+  }
+};
+
 
 app.post("/creatdata", async(req, res) => {
   let {name} =req.body
   let crudModel= new crudModel({
     name
   })
+ try{
   await crudModel.save()
-  res.status(201).send({message:"Insert Data"})
-});
+  res.status(201).send({message:"Data Inserted"})
+ }catch(error){
+res.status(500).send({message:"Data not Inserted", error})
+ }
+})
 
 
 app.get("/alldata", async (req, res) => {
@@ -40,6 +57,7 @@ app.patch("/updatedata/:id", async (req, res) => {
   res.status(200).send({message: "Get all Data", updatedata });
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, async (req, res) => {
   console.log(`Server is running http://localhost:${port}`);
+  await connectDB();
 });
